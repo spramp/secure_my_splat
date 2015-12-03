d732e79b48229c088376563e8ed46079c44abeeb
Jan Christian Bernabe <janchristianbernabe@gmail.com>
2015-12-03 12:23:41 -0600

    app.rb with connected database

--- app.rb
+++ app.rb
@@ -1,2 +1,13 @@
 require 'bundler'
 Bundler.require
+
+ActiveRecord::Base.establish_connection(
+  :database => 'bee_crypt_bzz',
+  :adapter => 'postgresql'
+)
+
+#basic template routes
+get '/' do
+  #return some resource
+  return {:hello => 'world'}.json
+end
